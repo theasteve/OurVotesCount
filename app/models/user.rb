4a5714ffa499a224788e1e978fcec50b587b1655
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-07-04 14:09:19 -0500

    add validations to User model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, email: true 
+end
