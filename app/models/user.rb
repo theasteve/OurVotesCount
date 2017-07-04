e0b44d9f82e8d0936ecd5212e296e99c20fdd46a
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-07-04 14:26:22 -0500

    refactor email validation to be in one line along first_name & last_name presence true

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :last_name, presence: true
-  validates :email, presence: true, email: true 
+  validates :first_name, :last_name, :emai,  presence: true
 end
