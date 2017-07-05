eb5f25a0f90c77b100091eabe63642f498946bd3
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-07-05 16:09:35 -0500

    add has_many through: association for the many-to-many relationship with users

---
+++ app/models/transportation.rb
@@ -0,0 +1,7 @@
+class transportation < ApplicationRecord
+  belongs_to :destination, class_name: :Protest
+  belongs_to :transporter, class_name: :User
+
+  has_many :passengers
+  has_many :users, through: :passengers
+end
