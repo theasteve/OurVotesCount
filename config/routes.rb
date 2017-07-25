231ff98718aa41d412b4a7650ae9cd55e0d39a82
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-07-25 12:10:11 -0400

    add nested route to display protests of a user

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
+
 resources :users, only: [:show] do
-  resources :protests
+  resources :protests, only: [:index]
 end
+
+  resources :protests
 
 end
