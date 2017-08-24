ed1b68bc0a7981f2318269e97a7402b573e73de0
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-08-24 15:44:42 -0400

    return json object which fixed no_head content 204 error

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,6 +5,8 @@ class AttendancesController < ApplicationController
     @protest = Protest.find(params[:protest_id])
     @attendance = Attendance.new(user_id: @user.id, protest_id: @protest.id)
     @attendance.save
+
+    @attendance.to_json
   end
 
   def destroy
