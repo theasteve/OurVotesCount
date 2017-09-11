5838705ddb61fefcf4886b931c115e18f707a255
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-09-11 14:39:39 -0400

    add transportation to the show action in protest controller

--- app/controllers/protests_controller.rb
+++ app/controllers/protests_controller.rb
@@ -8,6 +8,7 @@ class ProtestsController < ApplicationController
   def show
     @protest = Protest.find(params[:id])
     @user = current_user
+    @transportation = Transportation.find(params[:id])
   end
 
   def new
