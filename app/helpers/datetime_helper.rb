ce8bde523cbafc53ac65254cec62bf55e4cbb4e9
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2018-01-22 20:25:04 -0500

    refactor protests/index using bootstrap

--- app/helpers/datetime_helper.rb
+++ app/helpers/datetime_helper.rb
@@ -7,4 +7,16 @@ module DatetimeHelper
   def to_time(time)
     time.strftime("at %I:%M%p")
   end
+
+  def to_day(day)
+    day.strftime("%d")
+  end
+
+  def to_month(month)
+    month.strftime("%B")
+  end
+
+  def to_year(year)
+    year.strftime("%Y")
+  end
 end
