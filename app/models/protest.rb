a1bcb7e9536ed425cdd2f17bb93aaec2a1587dd7
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2018-04-15 21:01:39 -0400

    clean up event  model

--- app/models/protest.rb
+++ app/models/protest.rb
@@ -2,7 +2,10 @@ class Protest < ApplicationRecord
   validates :name, :description, :location,
             :starts_at, :creator, presence: true
 
-  has_attached_file :image, styles: {thumb: "100x100#", medium: "400x600#" }, default_url: ":style/missing.png"
+  has_attached_file :image, styles: {thumb: "100x100#",
+                                     medium: "400x600#" },
+                                     default_url: ":style/missing.png"
+                                     
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   belongs_to :creator, class_name: :User
